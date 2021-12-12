3594ac5eda187d0534116bd9e944ad6fd187dc4a
mrgs83 <40406835+mrgs83@users.noreply.github.com>
2021-12-12 21:28:23 +0200

    Update build_tools.sh

--- build_tools.sh
+++ build_tools.sh
@@ -13,6 +13,8 @@ cp sample_user_config.json apollolake_user_config.json ;
 ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/tg3/rpext-index.json ;
 ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/ixgbe/rpext-index.json ;
 ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/8139cp/rpext-index.json ;
+./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/e1000/rpext-index.json ;
+./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/e1000e/rpext-index.json ;
 #./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/8139too/rpext-index.json ;
 #./redpill_tool_chain.sh build apollolake-7.0.1-42218 ;
 #./redpill_tool_chain.sh auto apollolake-7.0.1-42218 ;
